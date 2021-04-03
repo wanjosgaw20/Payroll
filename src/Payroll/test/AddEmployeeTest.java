4afeb915134695507700868888d89e92168c0c56
wanjosgaw20 <xingrui19754@126.com>
2021-04-03 16:26:04 +0800

    为添加小时工建立测试

---
+++ src/Payroll/test/AddEmployeeTest.java
@@ -0,0 +1,14 @@
+package Payroll.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddEmployeeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
