459d98aeef9eddf8ccabaa5e5e1960d99669a1dc
wanjosgaw20 <xingrui19754@126.com>
2021-04-06 00:17:39 +0800

    添加构造方法

--- src/Payroll/exception/NosuchEmployeeException.java
+++ src/Payroll/exception/NosuchEmployeeException.java
@@ -2,9 +2,9 @@ package Payroll.exception;
 
 public class NosuchEmployeeException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	public NosuchEmployeeException(String message){
+		super(message);
+	}
 }
