a1fd69525c2008244921e788fd293533ff5226ad
wanjosgaw20 <xingrui19754@126.com>
2021-04-05 23:56:14 +0800

    新建删除雇员测试

---
+++ src/Payroll/test/DeleteEmployeeTest.java
@@ -0,0 +1,14 @@
+package Payroll.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DeleteEmployeeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
