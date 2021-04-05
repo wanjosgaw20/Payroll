1b94a31b49e7e0ba635e56b7aefaf6e40fc7b89e
wanjosgaw20 <xingrui19754@126.com>
2021-04-05 23:42:05 +0800

    添加构造函数

--- src/Payroll/trans/AddCommissionedEmployeeTransaction.java
+++ src/Payroll/trans/AddCommissionedEmployeeTransaction.java
@@ -4,6 +4,11 @@ import Payroll.PaymentClassification;
 
 public class AddCommissionedEmployeeTransaction extends AddEmployeeTransaction {
 
+	public AddCommissionedEmployeeTransaction(int empId, String name, String address, double salary,
+			double commissionRate) {
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	protected PaymentClassification getPaymentClassification() {
 		// TODO Auto-generated method stub
