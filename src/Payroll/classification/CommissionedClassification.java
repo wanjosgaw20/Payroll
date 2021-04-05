cbd6b2fee51dd06f58c166144dd2c894c5541e03
wanjosgaw20 <xingrui19754@126.com>
2021-04-05 23:52:02 +0800

    完成添加月薪加提成雇员

--- src/Payroll/classification/CommissionedClassification.java
+++ src/Payroll/classification/CommissionedClassification.java
@@ -4,14 +4,20 @@ import Payroll.PaymentClassification;
 
 public class CommissionedClassification extends PaymentClassification {
 
+	private double salary;
+	private double commissionRate;
+
+	public CommissionedClassification(double salary, double commissionRate) {
+		this.salary = salary;
+		this.commissionRate = commissionRate;
+	}
+
 	public double getSalary() {
-		// TODO Auto-generated method stub
-		return 0;
+		return salary;
 	}
 
 	public double getCommissionRate() {
-		// TODO Auto-generated method stub
-		return 0;
+		return commissionRate;
 	}
 
 }
--- src/Payroll/trans/AddCommissionedEmployeeTransaction.java
+++ src/Payroll/trans/AddCommissionedEmployeeTransaction.java
@@ -1,18 +1,24 @@
 package Payroll.trans;
 
 import Payroll.PaymentClassification;
+import Payroll.classification.CommissionedClassification;
 
 public class AddCommissionedEmployeeTransaction extends AddEmployeeTransaction {
 
+	private double salary;
+	private double commissionRate;
+
 	public AddCommissionedEmployeeTransaction(int empId, String name, String address, double salary,
 			double commissionRate) {
-		// TODO Auto-generated constructor stub
+		super(empId, name, address);
+		this.salary = salary;
+		this.commissionRate = commissionRate;
 	}
 
 	@Override
 	protected PaymentClassification getPaymentClassification() {
 		// TODO Auto-generated method stub
-		return null;
+		return new CommissionedClassification(salary,commissionRate);
 	}
 
 }
