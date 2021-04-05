bb6b815724b153a7653280d161ecf102cdd03988
wanjosgaw20 <xingrui19754@126.com>
2021-04-05 22:05:20 +0800

    修正编译错误，整理代码

--- src/Payroll/trans/AddHourlyEmployeeTransaction.java
+++ src/Payroll/trans/AddHourlyEmployeeTransaction.java
@@ -1,17 +1,11 @@
 package Payroll.trans;
 
-import Payroll.Employee;
 import Payroll.PaymentClassification;
-import Payroll.PayrollDatabase;
 import Payroll.Transaction;
 import Payroll.classification.HourlyClassification;
-import Payroll.method.HoldMethod;
 
-public class AddHourlyEmployeeTransaction implements Transaction {
+public class AddHourlyEmployeeTransaction extends AddEmployeeTransaction implements Transaction {
 
-	private int empId;
-	private String name;
-	private String address;
 	private double hourlyRate;
 
 	public  AddHourlyEmployeeTransaction(int empId, String name, String address, double hourlyRate) {
@@ -22,17 +16,6 @@ public class AddHourlyEmployeeTransaction implements Transaction {
 	}
 
 	@Override
-	public void execute() {
-		//新建雇员
-		Employee employee = new Employee(empId, name, address);
-		//设置工资计算方式
-		employee.setPaymentClassification(getPaymentClassification());
-		//设置工资支付方法
-		employee.setPaymentMethod(new HoldMethod());
-		//保存到数据库
-		PayrollDatabase.save(employee);
-	}
-
 	protected PaymentClassification getPaymentClassification() {
 		return new HourlyClassification(hourlyRate);
 	}
--- src/Payroll/trans/AddSalariedEmployeeTransaction.java
+++ src/Payroll/trans/AddSalariedEmployeeTransaction.java
@@ -1,17 +1,11 @@
 package Payroll.trans;
 
-import Payroll.Employee;
 import Payroll.PaymentClassification;
-import Payroll.PayrollDatabase;
 import Payroll.Transaction;
 import Payroll.classification.SalariedClassification;
-import Payroll.method.HoldMethod;
 
-public class AddSalariedEmployeeTransaction implements Transaction {
+public class AddSalariedEmployeeTransaction extends AddEmployeeTransaction implements Transaction {
 	
-	private int empId;
-	private String name;
-	private String address;
 	private double salary;
 
 	public AddSalariedEmployeeTransaction(int empId, String name, String address, double salary) {
@@ -22,21 +16,12 @@ public class AddSalariedEmployeeTransaction implements Transaction {
 			this.salary = salary;
 	}
 
-	@Override
-	public void execute() {
-		//新建雇员
-		Employee employee = new Employee(empId,name,address);
-		//设置工资计算方式
-		employee.setPaymentClassification(getPaymenClassification());
-		//设置工资支付方法
-		employee.setPaymentMethod(new HoldMethod());
-		//保存到数据库
-		PayrollDatabase.save(employee);
-
-	}
-
 	protected PaymentClassification getPaymenClassification() {
 		return new SalariedClassification(salary);
+	}
+
+	protected PaymentClassification getPaymentClassification() {
+		return null;
 	}
 
 }
