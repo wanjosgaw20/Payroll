b2e68c0e9728771fe196858762a7cc9938d9ce42
wanjosgaw20 <xingrui19754@126.com>
2021-04-05 23:45:10 +0800

    添加方法

--- src/Payroll/classification/CommissionedClassification.java
+++ src/Payroll/classification/CommissionedClassification.java
@@ -4,4 +4,14 @@ import Payroll.PaymentClassification;
 
 public class CommissionedClassification extends PaymentClassification {
 
+	public double getSalary() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getCommissionRate() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
